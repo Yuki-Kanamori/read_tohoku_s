8e607858c7335147af8903e2bece0b1cfeb9489a
Yuki Kanamori <Yuki@19-0164.local>
2020-04-09 11:52:53 +0900

    small changes

--- make_df.R
+++ make_df.R
@@ -176,14 +176,7 @@ for(i in 1:13){
              data %>% filter(rowSums(is_blank(.)) != ncol(.)) %>% 
                select_if(colSums(is_blank(.)) != nrow(.)))
     }
-    
-    # x = df %>%
-    #   # 空行の除去
-    #   filter(rowSums(is_blank(.)) != ncol(.)) %>%
-    #   # 空列の除去
-    #   select_if(colSums(is_blank(.)) != nrow(.))
-    # summary(x)
-    
+
     df1 = df1[-c(1:2), ] #number
     df2 = df2[-c(1), ] #station
     df3 = df3 #depth
@@ -208,71 +201,19 @@ for(i in 1:13){
       warning(paste('please check the objects'))
     }
   }
+  
+  
+  if(i > 12){
+    
+    
+    
+    
+    
+  }
 }
 
-ß
 
 
-
-
-
-
-
-
-
-
-
-
-
-for(i in 1:6){
-  # i = 3
-  file = file_list[i]
-  
-  for(j in 1:3){
-    assign(paste0('df', j),
-           read.csv(file, na.strings = NULL, fileEncoding = "CP932", skip = j))
-  }
-  
-  # for(j in 1:3){
-  #   name = paste0('df', j)
-  #   data = get(name)
-  #   assign(paste0('df', j),
-  #          colnames(data)[2] = 'X')
-  # }
-  
-  for(j in 1:3){
-    name = paste0('df', j)
-    data = get(name)
-    assign(paste0('df', j),
-           data %>% filter(rowSums(is_blank(.)) != ncol(.)) %>% 
-             select_if(colSums(is_blank(.)) != nrow(.)))
-  }
-  
-  df1 = df1[-c(1:3), ] #number
-  df2 = df2[-c(1:2), ] #station
-  df3 = df3[-1, ] #depth
-  
-  x1 = df1 %>% tidyr::gather(key = ymd, value = number, 3:ncol(df1)) %>% mutate(n_number = as.numeric(number)) %>% dplyr::rename(species = 年月日, net = X)
-  summary(x1)
-  x1[is.na(x1)] = 0
-  
-  x2 = df2 %>% tidyr::gather(key = station, value = number, 3:ncol(df2)) %>% mutate(n_number = as.numeric(number)) %>% dplyr::rename(species = STATIONコード, net = X)
-  summary(x2)
-  x1[is.na(x2)] = 0
-  
-  x3 = df3 %>% tidyr::gather(key = depth, value = number, 3:ncol(df1)) %>% mutate(n_number = as.numeric(number)) %>% dplyr::rename(species = 水深, net = X)
-  summary(x3)
-  x3[is.na(x3)] = 0
-  
-  if(nrow(x1)*3 - (nrow(x1)+nrow(x2)+nrow(x3)) == 0){
-    x = cbind(x1, x2 %>% select(station), x3 %>% select(depth)) %>% mutate(file_name = file)
-  }else{
-    warning(paste('please check the objects'))
-  }
-  
-  test = rbind(test, x)
-}
-unique(test$file_name)
 
 
 
